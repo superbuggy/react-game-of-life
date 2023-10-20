056769debc946740d128b47e5edb5712b1b26f5c
superbuggy <jpqreichard@gmail.com>
2023-10-20 18:56:57 -0400

    ♻️ Define state generatin in effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as Tone from "tone";
 import { useEffect, useState, FC } from "react";
-import { map, truncate, make2DArray } from "./utils";
+import { map, make2DArray, createSpiralArray, centsOff, noteHz } from "./utils";
 import "./App.css";
 
 type Grid = number[][];
@@ -12,6 +12,22 @@ interface GameState {
   population: number;
   hasStarted: boolean;
 }
+
+const countNeighbors = (grid: Grid, x: number, y: number) => {
+  const [columns, rows] = [grid[0].length, grid.length];
+  let sum = 0;
+  for (let i = -1; i <= 1; i++) {
+    for (let j = -1; j <= 1; j++) {
+      // The % operator treats opposing edges as being connected,
+      // like a sphere or a wrap-around plane...
+      const column = (x + i + columns) % columns;
+      const row = (y + j + rows) % rows;
+      if (x === column && y === row) continue;
+      sum += grid[column][row];
+    }
+  }
+  return sum;
+};
 
 const GameOfLife: FC = () => {
   const size: number = 7;
@@ -26,22 +42,6 @@ const GameOfLife: FC = () => {
     population: 0,
     hasStarted: false,
   });
-
-  const countNeighbors = (grid: Grid, x: number, y: number) => {    
-    const { columns, rows } = state;
-    let sum = 0;
-    for (let i = -1; i <= 1; i++) {
-      for (let j = -1; j <= 1; j++) {
-        // The % operator treats opposing edges as being connected,
-        // like a sphere or a wrap-around plane...
-        const column = (x + i + columns) % columns;
-        const row = (y + j + rows) % rows;
-        if (x === column && y === row) continue;
-        sum += grid[column][row];
-      }
-    }
-    return sum;
-  };
 
   const randomizeGrid = (initialGrid: Grid): Grid => {
     const grid = initialGrid.map((column) => column.slice());
@@ -62,33 +62,10 @@ const GameOfLife: FC = () => {
   };
 
   const countPopulation = (grid: Grid) => {
-    return grid.reduce((tally, row) => tally + row.reduce((tally, cell) => tally + cell, 0), 0);
-  };
-
-  const nextGridState = () => {
-    setState(({ grid, ...prevState }: GameState) => {
-      const nextGrid = grid.map((row, rowIndex) => {
-        return row.map((cell, cellIndex) => {
-          const neighbors = countNeighbors(grid, rowIndex, cellIndex);
-          if (cell === 0 && neighbors === 3) {
-            return 1;
-          } else if (cell === 1 && (neighbors < 2 || neighbors > 3)) {
-            return 0;
-          } else {
-            const negativeEntropy = Math.floor(Math.random() * 1.0002);
-            return cell || negativeEntropy;
-          }
-        });
-      });
-
-      const population = countPopulation(nextGrid);
-
-      return {
-        ...prevState,
-        grid: nextGrid,
-        population,
-      };
-    });
+    return grid.reduce(
+      (tally, row) => tally + row.reduce((tally, cell) => tally + cell, 0),
+      0
+    );
   };
 
   useEffect(() => {
@@ -105,22 +82,20 @@ const GameOfLife: FC = () => {
 
       // Create an instance of Oscillator for the "boom" sound
       const boomOscillator = new Tone.Oscillator({
-        type: "square", 
-        frequency: 50, 
-        volume: -20, 
+        type: "square",
+        frequency: 50,
+        volume: -20,
       });
-
 
       explosionSynth.toDestination();
       boomOscillator.toDestination();
 
-
       const now = Tone.now();
 
-      boomOscillator.start(now).stop(now + 0.4); 
+      boomOscillator.start(now).stop(now + 0.4);
       boomOscillator.frequency.rampTo(20, 0.4);
-      explosionSynth.triggerAttackRelease("4n"); 
-      
+      explosionSynth.triggerAttackRelease("4n");
+
       Tone.Transport.stop();
       setState((priorState) => ({ ...priorState, hasStarted: false }));
     }
@@ -131,63 +106,40 @@ const GameOfLife: FC = () => {
     initializeGrid();
     Tone.Transport.start();
     setState((priorState) => ({ ...priorState, hasStarted: true }));
-
-    // Create an instance of NoiseSynth
-
-    // Create an instance of NoiseSynth for the initial explosion
   };
-
-  // componentDidMount(): void {
-  //   console.log(Tone.Frequency(220).toNotation());
-  //   console.log(Tone.Frequency("C3"));
-  //   const playingEventId = Tone.Transport.scheduleRepeat(() => {
-  //     console.log(state.population);
-  //     nextGridState();
-  //   }, "1m");
-
-  //   const animatingEventId = Tone.Transport.scheduleRepeat(() => {
-  //     console.log(state.population);
-  //   }, "1m");
-  //   timerIds = [playingEventId, animatingEventId];
-  //   updateCSS();
-  // }
-
-  // componentWillUnmount(): void {
-  //   timerIds.forEach((id) => Tone.Transport.clear(id));
-  // }
-
-  // componentDidUpdate(): void {
-  //   if (state.population === 0 && state.grid.length) {
-  //     die();
-  //   }
-  // }
 
   const makeToneLattice = (grid: number[][]): number[][] => {
     const baseFrequency = 110;
     return grid.map((row: number[], rowIndex: number) => {
       const baseRowFrequency = baseFrequency * (3 / 2) ** rowIndex;
-      return row.map((_, columnIndex: number) => baseRowFrequency * (5 / 4) ** columnIndex);
+      return row.map(
+        (_, columnIndex: number) => baseRowFrequency * (5 / 4) ** columnIndex
+      );
     });
   };
   const toneLattice: Grid = makeToneLattice(make2DArray(size, size));
 
   useEffect(() => {
     const synth: Tone.PolySynth = new Tone.PolySynth().toDestination();
-    synth.volume.value = map(state.population, maxPopIsh, -24, -6, state.population);
+    synth.volume.value = map(
+      state.population,
+      maxPopIsh,
+      -24,
+      -6,
+      state.population
+    );
     const playTones = (frequencies: number[]) => {
-      console.log("==", frequencies);
+      console.log("==", frequencies.length);
       const sequence = new Tone.Sequence(
         (time, note) => {
           console.log("beep");
-
           synth.triggerAttackRelease(note, ".95m", time);
         },
         frequencies,
         `${frequencies.length}n`
       );
       sequence.loop = false;
-
-      return sequence.start(Tone.now()); // Prevents notes from getting dropped in a sequence
+      return sequence.start(); 
     };
 
     const playGrid = (grid: Grid) => {
@@ -202,14 +154,43 @@ const GameOfLife: FC = () => {
     playGrid(state.grid);
   }, [state.grid, maxPopIsh, state.population, toneLattice]);
 
-  const updateCSS = () => {
-    document.documentElement.style.setProperty("--cell-size", `${100 / state.rows}%`);
+  useEffect(() => {
+    const nextGridState = () => {
+      const nextGrid = (grid: Grid) =>
+        grid.map((row, rowIndex) => {
+          return row.map((cell, cellIndex) => {
+            const neighbors = countNeighbors(grid, rowIndex, cellIndex);
+            if (cell === 0 && neighbors === 3) {
+              return 1;
+            } else if (cell === 1 && (neighbors < 2 || neighbors > 3)) {
+              return 0;
+            } else {
+              const negativeEntropy = Math.floor(Math.random() * 1.0002);
+              return cell || negativeEntropy;
+            }
+          });
+        });
+      setState(({ grid: currentGrid, ...prevState }: GameState) => {
+        const newGrid = nextGrid(currentGrid);
+        const population = countPopulation(newGrid);
+
+        return {
+          ...prevState,
+          grid: newGrid,
+          population,
+        };
+      });
+    };
+
+    // const updateCSS = () => {
+    // document.documentElement.style.setProperty("--cell-size", `${100 / size}%`);
     // const hue = map(minPopIsh, maxPopIsh * 0.7, 268 * 1.3, 154, state.population);
     // const light = map(minPopIsh, maxPopIsh, 15, 35, state.population);
     // document.documentElement.style.setProperty("--background-color", `hsl(${hue}, 78%, ${light}%)`);
-  };
-  useEffect(() => {
-    updateCSS();
+    // };
+
+    // updateCSS();
+
     const playingEventId = Tone.Transport.scheduleRepeat(() => {
       nextGridState();
     }, "1m");
@@ -231,7 +212,10 @@ const GameOfLife: FC = () => {
       }
       return (
         <div key={`${rowIndex} ${cellIndex}`} className={className}>
-          {truncate(toneLattice[rowIndex]?.[cellIndex])}
+          <span>
+            {noteHz(toneLattice[rowIndex]?.[cellIndex])}
+            <sup>{centsOff(toneLattice[rowIndex]?.[cellIndex])}</sup>
+          </span>
         </div>
       );
     });
@@ -245,7 +229,9 @@ const GameOfLife: FC = () => {
 
   return (
     <>
-      <div className={"cells-container"}>{state.hasStarted ? gridElements : buttonElement}</div>
+      <div className={"cells-container"}>
+        {state.hasStarted ? gridElements : buttonElement}
+      </div>
       <p>{state.population}</p>
     </>
   );
